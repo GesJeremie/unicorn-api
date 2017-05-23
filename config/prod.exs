063d0df68c08fa0d55503114a89a92443c54281d
Ges Jérémie <ges.jeremie@gmail.com>
2017-05-23 03:58:45 +0300

    Setup prod database

--- config/prod.exs
+++ config/prod.exs
@@ -16,6 +16,12 @@ config :unicorn, Unicorn.Endpoint,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
+# Database
+config :unicorn, Unicorn.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
+
 # Do not print debug messages in production
 config :logger, level: :info
 
