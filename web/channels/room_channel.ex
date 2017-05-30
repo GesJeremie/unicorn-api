17af763b73205e2dd67c50c818e62a190d2d9d70
Ges Jérémie <ges.jeremie@gmail.com>
2017-05-30 09:58:22 +0300

    Dummy socket to test out the ember app

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -9,4 +9,14 @@ defmodule Unicorn.RoomChannel do
     {:error, %{reason: "unauthorized"}}
   end
 
+  def handle_in("new_song", params, socket) do
+    broadcast! socket, "new_song", params
+    {:noreply, socket}
+  end
+
+  def handle_in("new_device", params, socket) do
+    broadcast! socket, "new_device", params
+    {:noreply, socket}
+  end
+
 end
