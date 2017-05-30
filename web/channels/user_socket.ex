dc9751661e24fc76010893af330e55f56e634f01
Ges Jérémie <ges.jeremie@gmail.com>
2017-05-30 05:15:39 +0300

    Basic socket to test

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule Unicorn.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", Unicorn.RoomChannel
+  channel "room:*", Unicorn.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
