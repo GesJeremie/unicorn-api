e8ed299f21fed4c1b7bb57b8c869af47547eeb6e
Jeremie Ges <ges.jeremie@gmail.com>
2017-07-05 08:58:10 +0300

    Test controller user

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -1,32 +1,40 @@
 defmodule Unicorn.UserControllerTest do
   use Unicorn.ConnCase, async: true
 
+  @create_attributes ["token", "inserted-at", "updated-at"]
+  @show_attributes ["inserted-at", "updated-at"]
+
   test "create renders user object with token" do
     conn = build_conn()
-
-    conn = post conn, user_path(conn, :create)
+    conn = post conn, "/v1/users"
 
     response = json_response(conn, :created)
+    attributes = get_response_attributes(response)
 
-    IO.puts "here"
-    IO.inspect response["data"]["attributes"]
-
-
-    assert json_response(conn, :created) 
-    """
-    assert json_response(conn, 200) == %{
-      "todos" => [%{
-          "title" => todo.title,
-          "description" => todo.description,
-          "inserted_at" => Ecto.DateTime.to_iso8601(todo.inserted_at),
-          "updated_at" => Ecto.DateTime.to_iso8601(todo.updated_at)
-        }]
-      }
-    """
+    assert has_attributes?(attributes, @create_attributes)
   end
 
   test "show renders user object without token" do
-    
+    {:ok, %{:model => user}} = Unicorn.User.CreateAction.run()
+
+    conn = build_conn()
+    conn = get conn, user_path(conn, :show, user.id)
+
+    response = json_response(conn, 200)
+    attributes = get_response_attributes(response)
+
+    assert has_attributes?(attributes, @show_attributes)
+    assert !"token" in attributes  
+  end
+
+  def has_attributes?(attributes, required_attributes) do
+    Enum.all?(attributes, fn(attribute) -> 
+      attribute in required_attributes
+    end)
+  end
+
+  def get_response_attributes(response) do
+    Map.keys(response["data"]["attributes"])
   end
 
 end
